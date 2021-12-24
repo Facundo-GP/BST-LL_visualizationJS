779d485a0434fd4a8fa33775ec0f7b99a5507d32
Facundo-GP <facundoguiper@hotmail.com>
2021-12-24 16:42:28 -0300

    v2

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,11 @@ import {Lista, agregarNodoLista} from "./lista.js";
 import {BS_t , insertarABB} from "./arbol.js";
 import {borrarTodo} from "./reset.js";
 
+window.Lista = Lista;
+window.agregarNodoLista = agregarNodoLista;
+window.BS_t = BS_t;
+window.insertarABB = insertarABB;
+window.borrarTodo = borrarTodo;
+
 var lista = new Lista;
 var arbol = new BS_t;
